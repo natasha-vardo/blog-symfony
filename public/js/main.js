849cb6134fc1c79e4a0608d27be548572dd130aa
Natasha Vardomskaya <nati_v@inbox.ru>
2018-11-28 20:52:22 +0300

    add pagination and delete posts

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+const posts = document.getElementById('postdata');
+
+if (posts) {
+    posts.addEventListener('click', e => {
+        if (e.target.className === 'btn btn-danger delete-post') {
+            if (confirm('Are you sure?')) {
+                const id = e.target.getAttribute('data-id');
+
+                fetch(`/my-posts/delete/${id}`, {
+                    method: 'DELETE'
+                }).then(res => window.location.reload());
+            }
+        }
+    });
+}
